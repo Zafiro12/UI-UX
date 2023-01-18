856c7aecce0f2d4fa5b486541da3c9cc46748886
Zafiro12 <alexsanta1205@gmail.com>
2023-01-18 14:45:23 +0100

    refactor

--- multimedia/reproductor/main.js
+++ multimedia/reproductor/main.js
@@ -1,54 +1,78 @@
 const audio = document.getElementById("audio");
-    const progressBar = document.getElementById("progress-bar");
+const progressBar = document.getElementById("progress-bar");
 
-    const playButton = document.getElementById("play");
+const playButton = document.getElementById("play");
 
-    const stopButton = document.getElementById("stop");
-    const repeatButton = document.getElementById("repeat");
+const stopButton = document.getElementById("stop");
+const repeatButton = document.getElementById("repeat");
 
-    const backwardsButton = document.getElementById("backwards");
-    const fordwardsButton = document.getElementById("forward");
+const backwardsButton = document.getElementById("backwards");
+const forwardsButton = document.getElementById("forward");
 
-    const volume = document.getElementById("volume");
+const mute = document.getElementById("mute");
+const volume = document.getElementById("volume");
 
-    playButton.addEventListener("click", function () {
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    })
-
-    stopButton.addEventListener("click", function () {
-        audio.pause();
-        audio.childNodes.
-        audio.currentTime = 0;
-    })
-
-    repeatButton.addEventListener("click", function () {
-        audio.currentTime = 0;
+playButton.addEventListener("click", function () {
+    if (audio.paused) {
         audio.play();
-    })
-
-    backwardsButton.addEventListener("click", function () {
-        audio.currentTime -= 10;
-    })
-    fordwardsButton.addEventListener("click", function () {
-        audio.currentTime += 10;
-    })
-
-    volume.addEventListener("change", function(e) {
-    audio.volume = e.currentTarget.value / 100;
+    } else {
+        audio.pause();
+    }
 })
 
-    audio.addEventListener("timeupdate", function (event) {
-        const playImage = document.getElementById("play-image");
+stopButton.addEventListener("click", function () {
+    audio.pause();
+    audio.currentTime = 0;
+})
 
-        progressBar.setAttribute("value", (event.target.currentTime/event.target.duration).toString());
+repeatButton.addEventListener("click", function () {
+    audio.currentTime = 0;
+    audio.play();
+})
 
-        if (event.target.currentTime === event.target.duration || event.target.paused) {
-            playImage.setAttribute("src", "assets/player-play.svg");
-        } else if (!event.target.paused) {
-            playImage.setAttribute("src", "assets/player-pause.svg");
-        }
-    })
+backwardsButton.addEventListener("click", function () {
+    audio.currentTime -= 10;
+})
+forwardsButton.addEventListener("click", function () {
+    audio.currentTime += 10;
+})
+
+let prevVolume = 100;
+mute.addEventListener("click", function () {
+    if (audio.volume!==0) {
+        prevVolume = audio.volume;
+        audio.volume = 0;
+    } else {
+        audio.volume = prevVolume;
+    }
+    
+    const muteImage = document.getElementById("mute-image");
+
+    if (audio.volume === 0) {
+        muteImage.setAttribute("src", "assets/images/player-mute.svg");
+    } else {
+        muteImage.setAttribute("src", "assets/images/player-volume.svg");
+    }
+})
+volume.addEventListener("change", function (e) {
+    audio.volume = e.currentTarget.value / 100;
+    const muteImage = document.getElementById("mute-image");
+
+    if (audio.volume === 0) {
+        muteImage.setAttribute("src", "assets/images/player-mute.svg");
+    } else {
+        muteImage.setAttribute("src", "assets/images/player-volume.svg");
+    }
+})
+
+audio.addEventListener("timeupdate", function (event) {
+    const playImage = document.getElementById("play-image");
+
+    progressBar.setAttribute("value", (event.target.currentTime / event.target.duration).toString());
+
+    if (event.target.currentTime === event.target.duration || event.target.paused) {
+        playImage.setAttribute("src", "assets/images/player-play.svg");
+    } else if (!event.target.paused) {
+        playImage.setAttribute("src", "assets/images/player-pause.svg");
+    }
+})
